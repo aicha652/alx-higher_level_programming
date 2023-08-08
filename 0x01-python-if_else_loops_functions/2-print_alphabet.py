cd1d4f9496742accd84caf167a5911abe50fabb0
aicha652 <aichaazlf652@gmail.com>
2023-08-08 04:54:50 -0700

    alphabet game

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+             "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+             "w", "x", "y", "z"]
+for alphabet in alphabets:
+    print(alphabet, end="")
