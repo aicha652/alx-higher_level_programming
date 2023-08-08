063787010b31402ccae016071b12c298e9bb41d0
aicha652 <aichaazlf652@gmail.com>
2023-08-08 09:06:19 -0700

    fix error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 99:
-        print('{}{}, '.format(0, number) if number <= 9 else '{}, '.format(number), end='')
+        print('{}{}'.format(0, number) if number <= 9 else '{}'.format(number),
+              end=', ')
     elif number == 99:
         print(number)
