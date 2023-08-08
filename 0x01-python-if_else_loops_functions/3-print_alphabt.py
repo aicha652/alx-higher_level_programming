e33c93e911742d69c5bb257ad00186d6b8d01f08
aicha652 <aichaazlf652@gmail.com>
2023-08-08 05:16:15 -0700

    task 3

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+alphabets = ["a", "b", "c", "d", "f", "g", "h" ,"i", "j",
+             "k", "l", "m", "n", "o", "p", "r", "s", "t",
+             "u", "v", "w", "x", "y", "z"]
+for alphabet in alphabets:
+    print("{}".format(alphabet), end="")
