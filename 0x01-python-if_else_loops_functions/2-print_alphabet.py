0dc8c74522a7804b38b46c77d9eaa0fafd82d443
aicha652 <aichaazlf652@gmail.com>
2023-08-08 05:00:24 -0700

    fix error

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@ alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
              "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
              "w", "x", "y", "z"]
 for alphabet in alphabets:
-    print(alphabet, end="")
+    print(f"{alphabet}", end="")
