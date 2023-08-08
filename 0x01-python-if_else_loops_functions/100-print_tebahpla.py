5b11d06a9d995fabaf79458e1d985a0759dbc10c
aicha652 <aichaazlf652@gmail.com>
2023-08-08 14:54:19 -0700

    smile in the miror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 alphabets = ["z", "Y", "x", "W", "v", "U", "t", "S",
-             "r", "Q", "p", "O", "n", "M", "l", "k",
+             "r", "Q", "p", "O", "n", "M", "l", "K",
              "j", "I", "h", "G", "f", "E", "d", "C",
              "b", "A"]
 for alphabet in alphabets:
