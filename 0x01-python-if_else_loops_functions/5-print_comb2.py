d83560462ccbe3c8d3622bc5b2a88c674eb8ce8c
aicha652 <aichaazlf652@gmail.com>
2023-08-08 08:45:21 -0700

    fix error task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-numbers1 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-#numbers2 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-for number1 in numbers1:
-    for number2 in numbers1:
-        result = '{}{}'.format(number1, number2)
-        if (result == '99'):
-            print(result)
-        else:
-            print('{}{}, '.format(number1, number2), end='')
+for number in range(0, 100):
+    if number == 99:
+        print(number)
+    else:
+        print('{}, '.format(number), end='')
