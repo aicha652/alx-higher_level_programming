4b7b026e5c119c53c46c409043dfc4e92bccf429
aicha652 <aichaazlf652@gmail.com>
2023-08-09 13:46:17 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in range(len(str)):
+        if str[i] >= 'a' and str[i] <= 'z':
+            s = str[i]
+            s = ord(s)
+            s = s - 32
+            s = chr(s)
+            str = str[:i] + s + str[i+1:]
+    print(str)
+    return None
