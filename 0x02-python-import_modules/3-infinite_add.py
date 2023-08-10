a1c6920a8b83f504325ee0a3243430b9b581882d
aicha652 <aichaazlf652@gmail.com>
2023-08-10 13:47:13 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,14 +6,14 @@ args = len(sys.argv) - 1
 
 def main():
     if args == 0:
-        print('0')
+        print('{}'.format(args))
     else:
         index = 1
         sum = 0
         while(args >= index):
             sum = sum + int(sys.argv[index])
             index = index + 1
-        print(sum)
+        print('{}'.format(sum))
 
 
 if __name__ == "__main__":
