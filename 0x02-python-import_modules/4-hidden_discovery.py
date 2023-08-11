d00f9f1aae4ef7b4cdcf06e6fcfac25b66e38e52
aicha652 <aichaazlf652@gmail.com>
2023-08-11 04:49:05 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,11 @@
 import hidden_4
 
 
-def main():
+def hidden_funct():
     names = dir(hidden_4)
     for name in names:
         if (name[:2] != "__"):
             print(name)
 
 if __name__ == "__main__":
-    main()
+    hidden_funct()
