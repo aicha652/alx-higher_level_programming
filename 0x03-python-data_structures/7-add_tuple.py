79a0d8fa5f5c3575f58c42327c0e965bf119b1b6
aicha652 <aichaazlf652@gmail.com>
2023-08-15 10:05:36 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,6 +8,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     while len(tuple_b) < 2:
         tuple_b += (0,)
 
-    for i in range(0, len(tuple_a)):
+    for i in range(0, 2):
         new_tuple.append(tuple_a[i] + tuple_b[i])
     return tuple(new_tuple)
