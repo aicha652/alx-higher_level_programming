9d59acb6433733583ac356ad0f9f1274ec95b6c7
aicha652 <aichaazlf652@gmail.com>
2023-08-29 12:00:52 -0700

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,14 +16,9 @@ class Square:
         Args:
             size (int): Description of size
         """
-        try:
-            if size is not None:
-                if type(size) is not int:
-                    raise TypeError
-                if size < 0:
-                    raise ValueError
-                self._Square__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if size is not None:
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self._Square__size = size
