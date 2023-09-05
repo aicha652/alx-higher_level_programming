f5a79e03c347a28fe6c57387171fc63cc12e9322
aicha652 <aichaazlf652@gmail.com>
2023-09-05 11:46:36 -0700

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,9 +2,13 @@
 def matrix_divided(matrix, div):
     if type(matrix) != list:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        for i in row:
+            if type(i) != int and type(i) != float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if len(set(map(len,matrix))) != 1:
         raise TypeError("Each row of the matrix must have the same size")
-    if type(div) != int:
+    if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
