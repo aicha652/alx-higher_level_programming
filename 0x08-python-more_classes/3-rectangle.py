876eda0b09d45a29542ef359f4f5c4781b08247d
aicha652 <aichaazlf652@gmail.com>
2023-09-06 05:26:18 -0700

    fix pycodestyle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,51 +3,51 @@
 
 
 class Rectangle:
-      """A class that defines a Rectangle"""
-            
-      def __init__(self, width=0, height=0):
-          self.__width = width
-          self.__height = height
-      
-      @property
-      def width(self):
-          return self.__width
+    """A class that defines a Rectangle"""
 
-      @width.setter
-      def width(self, value):
-           if type(value) != int:
-              raise TypeError("width must be an integer")
-           if value < 0:
-              raise ValueError("width must be >= 0")
-           self.__width = value
-      
-      @property
-      def height(self):
-          return self.__height
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
 
-      @height.setter
-      def height(self, value):
-          if type(value) != int:
-             raise TypeError("height must be an integer")
-          if value < 0:
-             raise ValueError("height must be >= 0")
-          self.__height = value
+    @property
+    def width(self):
+        return self.__width
 
-      def area(self):
-          return self.__width * self.__height
-       
-      def perimeter(self):
-          if self.__width == 0 or self.__height == 0:
-             return 0
-          return 2 * self.__width + 2 * self.__height
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-      def __str__(self):
-          string = ""
-          if self.__width == 0 or self.__height == 0:
-             return string
-          for i in range(self.__height):
-              for j in range(self.__width):
-                  string += "#"
-              string += "\n"
-          string = string[:-1]
-          return string
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * self.__width + 2 * self.__height
+
+    def __str__(self):
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            return string
+        for i in range(self.__height):
+            for j in range(self.__width):
+                string += "#"
+            string += "\n"
+        string = string[:-1]
+        return string
