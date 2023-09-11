b779f9b63943b22ca882c105b7afc322aeb03f3c
aicha652 <aichaazlf652@gmail.com>
2023-09-11 06:03:58 -0700

    task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,6 +7,7 @@ otherwise False"""
 def is_same_class(obj, a_class):
     """Define a function"""
 
-    if not isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is a_class:
+        return True
+    else:
         return False
-    return True
