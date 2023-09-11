72d3a022bb28dad9cb203c8af434aca6eab9decb
aicha652 <aichaazlf652@gmail.com>
2023-09-11 05:35:40 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,9 @@
 
 class MyList(list):
 
+    """Class MyList that inherits from list"""
     def print_sorted(self):
+        """Public instance method that prints the list,
+        but sorted (ascending sort)"""
+
         print(sorted(self))
