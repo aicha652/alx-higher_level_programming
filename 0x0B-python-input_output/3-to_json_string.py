5516eb3f31c5a6410745a1044cbebda4e00a7966
aicha652 <aichaazlf652@gmail.com>
2023-09-12 04:43:11 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""Create a function that returns the JSON
+representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """Define a function"""
+
+    return json.dumps(my_obj)
