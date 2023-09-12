e1b9b51fde7c84c04b6f30d25fa08e69d0e67ffc
aicha652 <aichaazlf652@gmail.com>
2023-09-12 08:28:27 -0700

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Create a class Student that defines a student"""
+
+
+class Student:
+    """Define a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Public method that retrieves a dictionary
+        representation of a Student instance"""
+
+        return self.__dict__
