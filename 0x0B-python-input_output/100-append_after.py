1d17d3074cdeadf3da07332dd42547b5cf643046
aicha652 <aichaazlf652@gmail.com>
2023-09-12 14:13:14 -0700

    task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that inserts a line of text to a file,
+after each line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Define a function"""
+
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
