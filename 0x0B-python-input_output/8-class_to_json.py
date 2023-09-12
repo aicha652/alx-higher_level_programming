fce2ebef3fee61e77767e5f9745c8aa4c8c277a8
aicha652 <aichaazlf652@gmail.com>
2023-09-12 08:17:12 -0700

    fix requirement

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,10 +2,9 @@
 """function that returns the dictionary description with
 simple data structure (list, dictionary, string, integer
 and boolean) for JSON serialization of an object"""
-import json
 
 
 def class_to_json(obj):
     """Define a function"""
 
-    return (json.dumps(obj.__dict__))
+    return obj.__dict__
