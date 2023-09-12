c8747e536302e17378a664e3ad8834601025c750
aicha652 <aichaazlf652@gmail.com>
2023-09-12 13:00:22 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Create a class Student that defines a student"""
+
+
+class Student:
+    """Define a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method that retrieves a dictionary
+        representation of a Student instance"""
+
+        if type(attrs) is list:
+            return {attr: getattr(self, attr)
+                    for attr in attrs
+                    if hasattr(self, attr)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Public method that replaces all
+        attributes of the Student instance"""
+
+        for key, value in json.items():
+            setattr(self, key, value)
