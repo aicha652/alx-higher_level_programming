5dc62e0a97b86ab0695791609947df9d1268416c
aicha652 <aichaazlf652@gmail.com>
2023-11-13 06:45:10 -0800

    fix task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 let arg;
-if (!argv[2]) {
+if (!process.argv[2]) {
   arg = 'No argument';
 } else {
-  arg = argv[2];
+  arg = process.argv[2];
 }
 console.log(arg);
