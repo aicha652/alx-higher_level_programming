136285dc93bcb046f9ded810a93069d6c3f40bcd
aicha652 <aichaazlf652@gmail.com>
2023-11-14 11:50:35 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,9 @@
 const ImportSquare = require('./5-square');
 class Square extends ImportSquare {
   charPrint (c) {
-    if (!c) {
-      super.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('C');
-        }
-        console.log();
-      }
+    if (!c) c = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
