f54c51ac7292c5d9837205b63951b0a5f10a85ab
aicha652 <aichaazlf652@gmail.com>
2023-11-17 01:17:07 -0800

    fix

---
+++ 0x0F-python-object_relational_mapping/venv/1-filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import MySQLdb
+conn = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+# HERE I have to know SQL to grab all states in my database
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
---
+++ 0x0F-python-object_relational_mapping/venv/4-cities_by_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import MySQLdb
+conn = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_4_usa")
+cur = conn.cursor()
+cur.execute("SELECT cities.id, cities.name, states.name FROM cities,states \
+        WHERE states.id = cities.state_id ORDER BY cities.id ASC")
+# HERE I have to know SQL to grab all states in my database
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
