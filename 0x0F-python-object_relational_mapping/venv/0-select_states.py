bdf0c8d32f8a79401cbb15e6880048a4d7901990
aicha652 <aichaazlf652@gmail.com>
2023-11-17 01:21:08 -0800

    fix

--- 0x0F-python-object_relational_mapping/venv/0-select_states.py
+++ 0x0F-python-object_relational_mapping/venv/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import MySQLdb
-conn = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-# HERE I have to know SQL to grab all states in my database
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # HERE I have to know SQL to grab all states in my database
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
