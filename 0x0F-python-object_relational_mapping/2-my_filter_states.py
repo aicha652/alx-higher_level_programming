91afc5a8d9df11bf819468cc21c279dbb0890088
aicha652 <aichaazlf652@gmail.com>
2023-11-18 01:05:36 -0800

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY \
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
             id ASC".format(name)
     cur.execute(query)
     # HERE I have to know SQL to grab all states in my database
