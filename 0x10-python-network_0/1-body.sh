b72b2d375bea4a6b76f0f7405f728894dffbe68d
aicha652 <aichaazlf652@gmail.com>
2023-11-30 06:06:19 -0800

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-sh -c "[$(curl -o -I -L -s -w 'Route %{http_code}' $1) -eq 200]"
+curl -sL $1
