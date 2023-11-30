2fb267c088b088c9b36bb42b14cd7e7c916fc082
aicha652 <aichaazlf652@gmail.com>
2023-11-30 08:40:57 -0800

    fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s -X POST -d '{"name": "John Doe", "age": 33}' -H "Content-Type: application/json" $1
+curl -s -X POST -H "Content-Type: application/json" -d @$2 $1
