6be914ecdf34d618f3d92464f09e47c457dc7b3b
aicha652 <aichaazlf652@gmail.com>
2023-12-01 12:02:52 -0800

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
     response = requests.post(url, data={'email': email})
     print("Your email is: ", response.text)
