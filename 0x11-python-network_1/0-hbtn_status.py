232d6ade2b464c993182cb3f4280bbfe7ac93265
aicha652 <aichaazlf652@gmail.com>
2023-12-01 02:15:30 -0800

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("    - type:", html.__class__)
+        print("    - content:", html)
+        print("    - utf8 content:", html.decode("utf-8"))
