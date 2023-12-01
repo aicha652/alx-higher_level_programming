58a988c5dcc44faf4ab33ee7f29efb5914db8b30
aicha652 <aichaazlf652@gmail.com>
2023-12-01 12:43:16 -0800

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,4 +9,5 @@ if __name__ == "__main__":
     response = requests.get(url)
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
+        exit()
     print(response.text)
