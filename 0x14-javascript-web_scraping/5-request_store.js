948b82f036923bd536931f2851a58f626d0f57c5
aicha652 <aichaazlf652@gmail.com>
2024-01-03 09:18:09 -0800

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// script that gets the contents of a
+// webpage and stores it in a file
+
+// import the request module
+const request = require('request');
+
+// import the process module
+const process = require('process');
+
+// import FileSystem
+const fs = require('fs');
+
+// extract the url from the command-line arguments
+const url = process.argv[2];
+
+// extract the file path from the cmd-line arguemnts
+const filename = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) throw error;
+  else {
+    fs.writeFileSync(filename, body, 'utf-8');
+  }
+});
