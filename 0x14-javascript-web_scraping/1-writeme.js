f39a5f89bda4e05b9586d251514f7a9942198e65
aicha652 <aichaazlf652@gmail.com>
2024-01-03 01:26:34 -0800

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,9 +5,12 @@ const fs = require('fs');
 const process = require('process');
 
 // Regular Content of the file
-const filecontent = 'Python is cool';
+const filecontent = process.argv[3];
 
 // 1. Write a file with UTF-8
+// The first argument is the file path
+// The second argument is the string to write
+// The content of the file must be written in utf-8
 fs.writeFile(process.argv[2], filecontent, 'utf-8', (err) => {
   if (err) throw err;
 });
